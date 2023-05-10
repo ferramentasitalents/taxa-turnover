89e3b9c784c80ede6201a395a4ba5603fde86748
ferramentasitalents <133061937+ferramentasitalents@users.noreply.github.com>
2023-05-10 13:53:00 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -2,5 +2,5 @@ function calculateRate() {
   const numAceitas = document.getElementById("num-aceitas").value;
   const numFeitas = document.getElementById("num-feitas").value;
   const rate = (numAceitas / numFeitas * 100).toFixed(2);
-  document.getElementById("result").innerHTML = `Taxa de aceitação: ${rate}%`;
+  document.getElementById("result").innerHTML = `Taxa de turnover: ${rate}%`;
 }
