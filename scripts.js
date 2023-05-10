b9981c90742465b59a88d8103e4d9cdbf45074ff
ferramentasitalents <133061937+ferramentasitalents@users.noreply.github.com>
2023-05-10 13:48:23 -0300

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,6 @@
+function calculateRate() {
+  const numAceitas = document.getElementById("num-aceitas").value;
+  const numFeitas = document.getElementById("num-feitas").value;
+  const rate = (numAceitas / numFeitas * 100).toFixed(2);
+  document.getElementById("result").innerHTML = `Taxa de aceitação: ${rate}%`;
+}
