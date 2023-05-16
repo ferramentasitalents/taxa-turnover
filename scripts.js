23b043c2259c50b155c208c03885252a7d78d14f
ferramentasitalents <133061937+ferramentasitalents@users.noreply.github.com>
2023-05-15 23:13:12 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,6 +1,12 @@
-function calculateRate() {
-  const numAceitas = document.getElementById("num-aceitas").value;
-  const numFeitas = document.getElementById("num-feitas").value;
-  const rate = (numAceitas / numFeitas * 100).toFixed(2);
-  document.getElementById("result").innerHTML = `Taxa de turnover: ${rate}%`;
+function calculate() {
+  const hires = parseInt(document.getElementById("hires").value);
+  const terminations = parseInt(document.getElementById("terminations").value);
+
+  if (isNaN(hires) || isNaN(terminations)) {
+    document.getElementById("result").innerHTML = "Por favor, insira números válidos.";
+    return;
+  }
+
+  const turnover = ((terminations / hires) * 100).toFixed(2);
+  document.getElementById("result").innerHTML = `Taxa de turnover: ${turnover}%`;
 }
