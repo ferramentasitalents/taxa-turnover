d89550b804b18444d899e52060158510fd5cbae1
ferramentasitalents <133061937+ferramentasitalents@users.noreply.github.com>
2023-05-15 23:22:49 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,12 +1,6 @@
-function calculate() {
-  const hires = parseInt(document.getElementById("hires").value);
-  const terminations = parseInt(document.getElementById("terminations").value);
-
-  if (isNaN(hires) || isNaN(terminations)) {
-    document.getElementById("result").innerHTML = "Por favor, insira números válidos.";
-    return;
-  }
-
-  const turnover = ((terminations / hires) * 100).toFixed(2);
-  document.getElementById("result").innerHTML = `Taxa de turnover: ${turnover}%`;
+function calculateTurnoverRate() {
+  const numContratados = parseFloat(document.getElementById("num-contratados").value);
+  const numDesligamentos = parseFloat(document.getElementById("num-desligamentos").value);
+  const turnoverRate = (numDesligamentos / numContratados) * 100;
+  document.getElementById("result").innerHTML = `Taxa de Turnover: ${turnoverRate.toFixed(2)}%`;
 }
